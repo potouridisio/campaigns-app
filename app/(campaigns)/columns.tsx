a6c31856a8d89f33df69c652113fe9320418f663
Ioannis Potouridis <potouridisio@gmail.com>
2023-09-10 04:44:48 +0300

    feat: add actions column

--- app/(campaigns)/columns.tsx
+++ app/(campaigns)/columns.tsx
@@ -1,9 +1,36 @@
 'use client'
 
+import { MoreHorizontal } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { cn } from '@/lib/utils'
 import type { ColumnDef } from '@tanstack/react-table'
 
 import type { Campaign } from '../api/campaigns/campaign'
+
+const deleteOption = <DropdownMenuItem>Delete</DropdownMenuItem>
+
+const generateFilesOptions = (
+  <>
+    <DropdownMenuItem>Generate std file</DropdownMenuItem>
+    <DropdownMenuItem>Generate int file</DropdownMenuItem>
+  </>
+)
+
+const manageOptions = (
+  <>
+    <DropdownMenuItem>Manage quota</DropdownMenuItem>
+    <DropdownMenuItem>Manage throttle</DropdownMenuItem>
+  </>
+)
 
 export const columns: ColumnDef<Campaign>[] = [
   {
@@ -127,6 +154,43 @@ export const columns: ColumnDef<Campaign>[] = [
         day: '2-digit',
         year: 'numeric',
       })
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const campaign = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="h-8 w-8 p-0" variant="ghost">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            {campaign.status === 'C' ? generateFilesOptions : null}
+            {campaign.status === 'D' ? deleteOption : null}
+            {campaign.status === 'L' ? (
+              <>
+                {generateFilesOptions}
+                {manageOptions}
+              </>
+            ) : null}
+            {campaign.status === 'P' ? (
+              <>
+                <DropdownMenuItem>Mark complete</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                {generateFilesOptions}
+                {manageOptions}
+              </>
+            ) : null}
+            {campaign.status === 'R' ? deleteOption : null}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
     },
   },
 ]
